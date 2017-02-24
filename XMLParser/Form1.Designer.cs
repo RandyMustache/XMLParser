f2c51f3220d26bfc14055e70ccc53f20a08345de
RandyMustache <mitchellsmail13.ms@gmail.com>
2017-02-24 16:10:19 +1100

    Added an icon

--- XMLParser/Form1.Designer.cs
+++ XMLParser/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.rtbText = new System.Windows.Forms.RichTextBox();
@@ -97,8 +98,9 @@
             this.Controls.Add(this.rtbText);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "XML Parser";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
